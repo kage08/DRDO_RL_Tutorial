3cb2bbc6e74fe2192bf347d4b880811accf308a1
Harshavardhan P K <harshavardhan864.hk@gmail.com>
2019-11-08 00:42:53 +0530

    Final Draft

--- Q_SARSA/grid_world.py
+++ Q_SARSA/grid_world.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import colors
 
-UP = 0
-DOWN = 1
+DOWN = 0
+UP = 1
 LEFT = 2
 RIGHT = 3
 
@@ -53,7 +53,7 @@ class GridWorldEnv(object):
     '''
     def reset(self, start_state=None, goal_state=None):
         self.start_state = self.state = self.choose_state(self.start_states) if start_state is None else start_state
-        self.goal = self.choose_state(self.goal_states) if goal_state is None else goal_state
+        #self.goal = self.choose_state(self.goal_states) if goal_state is None else goal_state
         self.done = False
         self.steps = 0
         return self.state
@@ -74,9 +74,9 @@ class GridWorldEnv(object):
             action = self.action_space[self.random_generator.randint(len(self.action_space))]
 
         if action == UP:
-            new_state = (self.state[0]-1, self.state[1])
-        elif action == DOWN:
             new_state = (self.state[0]+1, self.state[1])
+        elif action == DOWN:
+            new_state = (self.state[0]-1, self.state[1])
         elif action == LEFT:
             new_state = (self.state[0], self.state[1]-1)
         elif action == RIGHT:
@@ -85,7 +85,7 @@ class GridWorldEnv(object):
         if self.is_in_grid(new_state[0], new_state[1]):
             self.state = new_state
         
-        if self.state == self.goal:
+        if self.state in self.goal_states:
             self.reward = self.goal_reward
             self.done = True
             return self.state, self.reward, self.done
@@ -102,11 +102,13 @@ class GridWorldEnv(object):
     '''
     def render(self, render_agent = False, ax = None):
         grid = self.grid.copy()
-        grid[self.start_state[0], self.start_state] = 3
-        grid[self.goal[0], self.goal[1]] = 4
+        for start in self.start_states:
+            grid[start[0], start[1]] = 3
+        for goal in self.goal_states:
+            grid[goal[0], goal[1]] = 4
         if render_agent:
             grid[self.state[0], self.state[1]] = 5
-        grid = np.flip(grid, 0)
+        
         plt.clf()
         if not render_agent:
             cmap = colors.ListedColormap(['#F5E5E1', '#F2A494', '#FF2D00', '#0004FF', '#00FF23'])
@@ -129,7 +131,7 @@ Grid World environment with leftward wind: extra left action with 0.5 probabilit
 '''
 class GridWorldWindyEnv(GridWorldEnv):
     def __init__(self, grid_file, start_states = [(0,0)], goal_states = [(10,10)], goal_reward = 10, max_steps = 100,
-                    action_fail_prob = 0.1, seed = None, windy_probab = 0.5):
+                    action_fail_prob = 0.0, seed = None, windy_probab = 0.5):
         super(GridWorldWindyEnv, self).__init__(grid_file, start_states, goal_states , goal_reward , max_steps,
                     action_fail_prob , seed)
         self.windy_probab = windy_probab
@@ -137,5 +139,27 @@ class GridWorldWindyEnv(GridWorldEnv):
     def step(self, action):
         ans = super().step(action)
         if not self.done and self.random_generator.rand() < self.windy_probab:
-            ans = super().step(LEFT)
+            ans1 = super().step(LEFT)
+            return ans1[0], ans[1]+ans1[1], ans1[2]
         return ans
+
+def plot_Q(Q, message = "Q plot"):
+    
+    plt.figure(figsize=(10,10))
+    plt.title(message)
+    plt.pcolor(Q.max(-1), edgecolors='k', linewidths=2)
+    plt.colorbar()
+    def x_direct(a):
+        if a in [UP, DOWN]:
+            return 0
+        return 1 if a == RIGHT else -1
+    def y_direct(a):
+        if a in [RIGHT, LEFT]:
+            return 0
+        return 1 if a == UP else -1
+    policy = Q.argmax(-1)
+    policyx = np.vectorize(x_direct)(policy)
+    policyy = np.vectorize(y_direct)(policy)
+    idx = np.indices(policy.shape)
+    plt.quiver(idx[1].ravel()+0.5, idx[0].ravel()+0.5, policyx.ravel(), policyy.ravel(), pivot="middle", color='red')
+    plt.show()
